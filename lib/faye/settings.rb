c73a7cd781389d3c3cdcaf668f62ca54ed1f1467
Mohammad El-Abid <mohammad.elabid@gmail.com>
2011-05-19 20:19:08 -0400

    Use local IP instead of localhost, this allows other LAN devices to connect (not usable on a live site)

--- lib/faye/settings.rb
+++ lib/faye/settings.rb
@@ -1,3 +1,10 @@
 require 'uri'
+require 'socket'
+
+# Ideally this would go elsewhere, but it's only used for Faye
+def local_ip
+  UDPSocket.open {|s| s.connect('64.233.187.99', 1); s.addr.last }
+end
+
 FAYE_TOKEN = "some-string"
-FAYE_URI = URI.parse("http://localhost:9292/faye")
+FAYE_URI = URI.parse("http://#{local_ip}:9292/faye")
